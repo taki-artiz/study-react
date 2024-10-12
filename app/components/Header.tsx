c0f473764d7dcbc69fb27096555b90565a9b9d34
h.taki <onionn2003@gmail.com>
2024-10-12 22:59:04 +0900

    ✨ ヘッダーの追加

---
+++ app/components/Header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import styles from "./Header.module.scss"
+
+export function Header(){
+  return(
+    <header className={styles.header}>
+      {/* <div className={styles.testStyle}></div> */}
+        <Link href="./">
+            Index
+        </Link>
+        <Link href="./about/">
+            About
+        </Link>
+      </header>
+  );
+}
